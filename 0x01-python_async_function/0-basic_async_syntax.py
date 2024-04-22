05d648caf9188fc09ae0a22a7ed9d2c3123d733c
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-04-22 17:07:48 +0100

    adding tasks .0 - .2

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''The basics of async'''
+
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronously generates a random float value within the range [0, max_delay].
+
+    Parameters:
+    max_delay (int): The maximum delay value. Defaults to 10.
+
+    Returns:
+    float: A random float value within the range [0, max_delay].
+    """
+    return random.uniform(0, max_delay)
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''Let's execute multiple coroutines at the same time with async'''
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Asynchronously spawns 'n' instances of 'wait_random' coroutine with the specified 'max_delay',
+    gathers their results concurrently, and returns a sorted list of delay values.
+
+    Parameters:
+        n (int): The number of times to spawn 'wait_random' coroutines.
+        max_delay (int): The maximum delay value for each 'wait_random' coroutine.
+
+    Returns:
+        List[float]: A list of delay values (floats) in ascending order.
+
+    '''
+    tasks = [wait_random(max_delay) for _ in range(0, n)]
+    delays = []
+    delays = await asyncio.gather(*tasks)
+
+    return sorted(delays)
